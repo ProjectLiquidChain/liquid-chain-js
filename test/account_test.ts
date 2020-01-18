e00ee9684954de65c5327d8c50e9ab0204e2700b
Kha Do <kha.do@quoine.com>
2020-01-19 01:16:36 +0700

    tests: Add nyc

--- test/account_test.ts
+++ test/account_test.ts
@@ -7,7 +7,7 @@ should();
 describe('Account', function () {
   describe('#fromString', function () {
     it('should return account', function () {
-      const a = Account.fromString('LDH4MEPOJX3EGN3BLBTLEYXVHYCN3AVA7IOE772F3XGI6VNZHAP6GX5R');
+      const a = Account.fromString('LAH4MEPOJX3EGN3BLBTLEYXVHYCN3AVA7IOE772F3XGI6VNZHAP6GX5R');
       a.getPublicKey().compare(Buffer.from('cfc611ee4df64337615866b262f53e04dd82a0fa1c4fff45ddcc8f55b9381fe3', 'hex')).should.equal(0);
       expect(a.getPrivateKey()).to.be.undefined;
     });
